b3ce4bae5e4ae5bd8ac5448c5dab2db5e4ec6071
Luciano Ramello <luchosr@gmail.com>
2024-09-03 15:33:23 +0200

    feat: adds budget validation

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,4 +1,13 @@
+import { useMemo, useState } from 'react';
+
 export default function BudgetForm() {
+  const [budget, setBudget] = useState(0);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBudget(event.target.valueAsNumber);
+  };
+
+  const isValid = useMemo(() => isNaN(budget) || budget <= 0, [budget]);
   return (
     <form className="space-y-5">
       <div className="flex flex-col space-y-5">
@@ -14,12 +23,15 @@ export default function BudgetForm() {
           className="w-full bg-white border border-gray-200 p-2"
           placeholder="define tu presupuesto"
           name="budget"
+          value={budget}
+          onChange={handleChange}
         />
       </div>
       <input
         type="text"
         value="Definir presupuesto"
-        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase"
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40"
+        disabled={isValid}
       />
     </form>
   );
