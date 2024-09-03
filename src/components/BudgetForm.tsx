b141aab81146306e548db23f1490fd1ee076c79e
Luciano Ramello <luchosr@gmail.com>
2024-09-03 12:27:59 +0200

    feat: adds budget form structure

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,3 +1,26 @@
 export default function BudgetForm() {
-  return <div>BudgetForm</div>;
+  return (
+    <form className="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label
+          htmlFor="budget"
+          className="text-4xl text-blue-600 font-bold text-center"
+        >
+          Definir Presupuesto
+        </label>
+        <input
+          type="number"
+          id="budget"
+          className="w-full bg-white border border-gray-200 p-2"
+          placeholder="define tu presupuesto"
+          name="budget"
+        />
+      </div>
+      <input
+        type="text"
+        value="Definir presupuesto"
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase"
+      />
+    </form>
+  );
 }
