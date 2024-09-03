f2df650af731718b15e4ace23adad4666fcf42a2
Luciano Ramello <luchosr@gmail.com>
2024-09-03 12:28:22 +0200

    style: improves header styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import BudgetForm from './components/BudgetForm';
 function App() {
   return (
     <>
-      <header className="bg-blue-600 py-8 max-h-72">
+      <header className="bg-blue-600 py-8 max-h-60">
         <h1 className="uppercase text-center font-black text-4xl text-white">
           Planificador de gastos
         </h1>
