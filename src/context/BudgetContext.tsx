024a482256c20f98ac048821911635180cf6d52e
Luciano Ramello <luchosr@gmail.com>
2024-09-03 19:48:14 +0200

    feat: implements context

---
+++ src/context/BudgetContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, useReducer } from 'react';
+import {
+  BudgetActions,
+  budgetReducer,
+  BudgetState,
+  initialState,
+} from '../reducers/budget-reducer';
+
+type BudgetContextProps = {
+  state: BudgetState | undefined;
+  dispatch: React.Dispatch<BudgetActions>;
+};
+
+type BudgetProviderProps = {
+  children: React.ReactNode;
+};
+
+export const BudgetContext = createContext<BudgetContextProps>(null!);
+
+export const BudgetProvider = ({ children }: BudgetProviderProps) => {
+  const [state, dispatch] = useReducer(budgetReducer, initialState);
+
+  return (
+    <BudgetContext.Provider value={{ state, dispatch }}>
+      {children}
+    </BudgetContext.Provider>
+  );
+};
