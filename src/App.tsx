b0061b42eb2482966ccb1220f0fe635921a5b8a4
Luciano Ramello <luchosr@gmail.com>
2024-09-04 18:31:49 +0200

    feat: adds Budget tracker to layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { useMemo } from 'react';
 import BudgetForm from './components/BudgetForm';
+import { useBudget } from './hooks/useBudget';
+import BudgetTracker from './components/Budgettracker';
 
 function App() {
+  const { state } = useBudget();
+
+  const isValidBudget = useMemo(() => state.budget > 0, [state.budget]);
   return (
     <>
       <header className="bg-blue-600 py-8 max-h-60">
@@ -8,7 +14,7 @@ function App() {
           Planificador de gastos
         </h1>
         <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-          <BudgetForm />
+          {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
         </div>
       </header>
     </>
