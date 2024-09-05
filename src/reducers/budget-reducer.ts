d2cab3a971482801e75a8e50bdcc7383ce5e09a8
Luciano Ramello <luchosr@gmail.com>
2024-09-05 09:17:02 +0200

    feat: implements show modal action

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,14 +1,18 @@
-export type BudgetActions = {
-  type: 'add-budget';
-  payload: { budget: number };
-};
+export type BudgetActions =
+  | {
+      type: 'add-budget';
+      payload: { budget: number };
+    }
+  | { type: 'show-modal' };
 
 export type BudgetState = {
   budget: number;
+  modal: boolean;
 };
 
 export const initialState: BudgetState = {
   budget: 0,
+  modal: false,
 };
 
 export const budgetReducer = (
@@ -19,6 +23,13 @@ export const budgetReducer = (
     return {
       ...state,
       budget: action.payload.budget,
+    };
+  }
+
+  if (action.type === 'show-modal') {
+    return {
+      ...state,
+      modal: true,
     };
   }
   return state;
