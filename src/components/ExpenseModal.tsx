2637372e1ea21765ec2fa200d7af3dc65d398cec
Luciano Ramello <luchosr@gmail.com>
2024-09-05 11:33:43 +0200

    feat: implements close modal action on modal

--- src/components/ExpenseModal.tsx
+++ src/components/ExpenseModal.tsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react';
 import { PlusCircleIcon } from '@heroicons/react/24/solid';
 import { Dialog, Transition } from '@headlessui/react';
 import { useBudget } from '../hooks/useBudget';
+import ExpenseForm from './ExpenseForm';
 
 export default function ExpenseModal() {
   const { state, dispatch } = useBudget();
@@ -14,7 +15,11 @@ export default function ExpenseModal() {
       </div>
 
       <Transition appear show={state.modal} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={() => {}}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => dispatch({ type: 'close-modal' })}
+        >
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -38,7 +43,9 @@ export default function ExpenseModal() {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-3xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"></Dialog.Panel>
+                <Dialog.Panel className="w-full max-w-3xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <ExpenseForm />
+                </Dialog.Panel>
               </Transition.Child>
             </div>
           </div>
