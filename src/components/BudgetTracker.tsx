086c1869fba14277bbec3d257da244bc5e766cb1
Luciano Ramello <luchosr@gmail.com>
2024-09-09 19:01:59 +0200

    feat: adds dynamical budget calculation

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,6 +1,16 @@
+import { useMemo } from 'react';
+import { useBudget } from '../hooks/useBudget';
 import AmountDisplay from './AmountDisplay';
 
 export default function BudgetTracker() {
+  const { state } = useBudget();
+
+  const totalExpenses = useMemo(
+    () => state.expenses.reduce((total, expense) => total + expense.amount, 0),
+    [state.expenses]
+  );
+
+  const remainingBudget = state.budget - totalExpenses;
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className="flex justify-center">
@@ -13,9 +23,9 @@ export default function BudgetTracker() {
         >
           Resetear App
         </button>
-        <AmountDisplay label="Presupuesto" amount="300" />
-        <AmountDisplay label="Disponible" amount="200" />
-        <AmountDisplay label="Gastado" amount="100" />
+        <AmountDisplay label="Presupuesto" amount={state.budget} />
+        <AmountDisplay label="Disponible" amount={remainingBudget} />
+        <AmountDisplay label="Gastado" amount={totalExpenses} />
       </div>
     </div>
   );
