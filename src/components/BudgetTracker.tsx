d58c7e241d36bebe49d528a0bc938ee3706add5d
Luciano Ramello <luchosr@gmail.com>
2024-09-10 09:54:27 +0200

    feat: implements  reset app on click

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -4,7 +4,7 @@ import AmountDisplay from './AmountDisplay';
 import 'react-circular-progressbar/dist/styles.css';
 
 export default function BudgetTracker() {
-  const { state, totalExpenses, remainingBudget } = useBudget();
+  const { state, dispatch, totalExpenses, remainingBudget } = useBudget();
 
   const percentage = +((totalExpenses / state.budget) * 100).toFixed(2);
   return (
@@ -25,6 +25,7 @@ export default function BudgetTracker() {
         <button
           type="button"
           className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
+          onClick={() => dispatch({ type: 'reset-app' })}
         >
           Resetear App
         </button>
