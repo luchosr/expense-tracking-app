ba16257329c11ab9be4ffd29934437eccb58c7f1
Luciano Ramello <luchosr@gmail.com>
2024-09-10 11:50:08 +0200

    feat: improves expense list filter

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -5,7 +5,16 @@ import ExpenseDetail from './ExpenseDetail';
 export default function ExpenseList() {
   const { state } = useBudget();
 
-  const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses]);
+  const filteredExpenses = state.currentCategory
+    ? state.expenses.filter(
+        (expense) => expense.category === state.currentCategory
+      )
+    : state.expenses;
+
+  const isEmpty = useMemo(
+    () => filteredExpenses.length === 0,
+    [state.expenses]
+  );
   return (
     <div className="mt-10 bg-white shadow-lg rounded-lg p-10">
       {isEmpty ? (
@@ -16,7 +25,7 @@ export default function ExpenseList() {
             Listado de gastos.
           </p>
 
-          {state.expenses.map((expense) => (
+          {filteredExpenses.map((expense) => (
             <ExpenseDetail key={expense.id} expense={expense} />
           ))}
         </>
